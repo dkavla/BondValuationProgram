fcc31094779128f8bf9839d3b081468f7fc220b5
Dawid Kowalczyk <dkowalczyk135@gmail.com>
2022-03-02 17:30:17 -0500

    Define user specific tests in main function

--- BondValuationProgram/src/main.cpp
+++ BondValuationProgram/src/main.cpp
@@ -10,6 +10,22 @@ using std::endl;
 
 int main() {
 
-	
+	double par = 1000.0;
+	double couponRate = 8.0;
+	int timeToMaturity = 5;
+	int freq = 2;
+	double marketPrice = 922.78;
+
+	Bond b1(timeToMaturity, freq, couponRate, par, marketPrice);
+
+	cout.setf(std::ios::fixed);
+	cout.setf(std::ios::showpoint);
+	cout.precision(2);
+	std::cout << "YTM: " << b1.yieldToMaturity() << "%" << std::endl;
+	std::cout << "Market Value: $" << b1.price() << std::endl;
+	std::cout << "Macaulay Duration: " << b1.macaulayDuration() << std::endl;
+	std::cout << "Modified Duration: " << b1.modifiedDuration() << "%" << std::endl;
+
+
 	return 0;
 }
